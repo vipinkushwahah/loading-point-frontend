b633af97959db67e05ac11ad28d1fc8453f8a240
Vipin Kushwaha <vipinkushwaha@Vipins-MacBook-Air.local>
2026-06-19 07:21:42 +0530

    added backen url to frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://loading-point-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
